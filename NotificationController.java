56fc41cee24e9d5ec6745ea522a9f18547193dfc
Raunak Mukhia <raunak.mukhia@gmail.com>
2018-04-20 23:18:04 +0700

    NotificationController format

--- NotificationController.java
+++ NotificationController.java
@@ -8,55 +8,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-
 @RestController
 public class NotificationController {
     @Autowired
     NotificationService notificationService;
-    @PostMapping(path = "/refresh_token", consumes = "/application/json",produces = "application/json")
 
+    @PostMapping(path="/refresh_token",
+            consumes = "application/json", produces = "application/json")
     String refreshToken(
-            @RequestBody String user_id,
-            @RequestBody String Token)
-    { System.out.println(user_id);        //boolean result = notificationService.refreshToken(user_id,token);
-        //if(result)
-            return "{ status: \"success\" }";
-        //else
-          //  return "{ status: \"failed\" }";
-
-    }
-    @Param("order_jwt") String order_jwt,
-    @Param("user_id") String user_id,
-    @Param("signature") String signature)
+            @RequestBody String json)
     {
-        System.out.println(order_jwt);
-        JSONObject orderJSON;
-        String result = "";
         try {
-            //User user = entityManager.find(User.class, "st119630");
-            String parsedJWT= jwtService.verifyJWT(order_jwt);
-            if( parsedJWT != null){
-                orderJSON = new JSONObject(parsedJWT);
-                Order orderObject = jsonService.jsonOrderToObject(orderJSON);
-                System.out.println(orderObject.getProducts());
-                result = orderService.processOrder(orderObject,user_id, signature);
-                //type
-                //int, boolean, real num
-                //develop
-                //type checking
-                //instructions (usual)
-                //assignment condition loop
-                //function call (param basic type)
-            }
-            else{
-            }
-            //oauth authen filter
-        }catch(JSONException e){
+            JSONObject request = new JSONObject(json);
+            String user_id = request.get("user_id").toString();
+            String token = request.get("token").toString();
+            System.out.println(user_id);
+            System.out.println(token);
+        } catch (JSONException e) {
             e.printStackTrace();
-            return "";
+            return "{ status: 'failed' }";
         }
-        return result;
+
+        return "{ status: 'success' }";
+    }
+
 }
